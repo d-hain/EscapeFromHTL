23c2e352b6950bf543db3f8baaa3b80d5ae0a660
d-hain <d.hain@gmx.at>
2022-08-04 23:27:04 +0200

    added new sprites player animations (nearly everything working) main menu complete settings menu complete credits in progress (will be done near the end of the game)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,41 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlayerController : MonoBehaviour {
-    public float moveSpeed;
-
     private Rigidbody2D _rb;
     private Vector2 _moveDirection;
+    private Animator _animator;
+    private int _animHorizontalID;
+    private int _animVerticalID;
+    private int _animHorizontalIdleID;
+    private int _animVerticalIdleID;
+    private int _animSpeedID;
+    
+    public float moveSpeed;
 
     void Start() {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _animHorizontalID = Animator.StringToHash("Horizontal");
+        _animVerticalID = Animator.StringToHash("Vertical");
+        _animHorizontalIdleID = Animator.StringToHash("HorizontalIdle");
+        _animVerticalIdleID = Animator.StringToHash("VerticalIdle");
+        _animSpeedID = Animator.StringToHash("Speed");
     }
 
     private void Update() {
         ProcessInputs();
+        //TODO: still playing idle anim when moving
+        if(_moveDirection.x != 0) {
+            _animator.SetFloat(_animHorizontalIdleID, _moveDirection.x);
+        }
+        if(_moveDirection.y != 0) {
+            _animator.SetFloat(_animVerticalIdleID, _moveDirection.y);
+        }
+        _animator.SetFloat(_animHorizontalID, _moveDirection.x);
+        _animator.SetFloat(_animVerticalID, _moveDirection.y);
+        _animator.SetFloat(_animSpeedID, _moveDirection.sqrMagnitude);
     }
 
     void FixedUpdate() {
@@ -24,8 +48,11 @@ public class PlayerController : MonoBehaviour {
 
         _moveDirection = new Vector2(moveX, moveY).normalized;
     }
-    
+
     private void Move() {
         _rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+        SetAnimMoveSpeed(_rb.velocity);
     }
+
+    private void SetAnimMoveSpeed(Vector2 velocity) { }
 }
